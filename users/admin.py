b0bc91f4d83c5a90a45acf0721096c46a114df26
Marty <40641054+mrichman94@users.noreply.github.com>
2018-08-09 19:33:20 +0100

    fix user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'username',]
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
-class User(AbstractBaseUser, PermissionsMixin):
-    user_name = models.CharField(max_length=255, unique=True)
+class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    is_staff = models.BooleanField(default=True)
-
-    USERNAME_FIELD = 'user_name'
+    def __str__(self):
+        return self.user_name
